052dc4280ea5a4d2690f83678ad20a33d124add1
hieutran200120 <tieucaca097@gmail.com>
2024-03-24 22:07:00 +0700

    page detail

--- mynew/urls.py
+++ mynew/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from api.views import APIViews
+from django.urls import path,include
+# from api.views import APIViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/data/', APIViews.as_view(), name='api_data'),
-    path('', APIViews.as_view()),  # Define a default view for the empty path
+    path('',include('api.urls'))
+    # path('', APIViews.as_view()),
 ]
 
