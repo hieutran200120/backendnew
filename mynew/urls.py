c63a74213f518c55ad4a444ee83cc008f7b4a0b0
QuangCuongMTA <nqcuong0312@gmail.com>
2024-03-25 22:30:15 +0700

    new

--- mynew/urls.py
+++ mynew/urls.py
@@ -21,6 +21,8 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('api.urls')),
-    path('ctxh/',include('ctxh.urls'))
+    path('ctxh/',include('ctxh.urls')),
+     path('chamsoctre/',include('chamsoctre.urls')),
+      path('ldvl/',include('ldvl.urls'))
 ]
 
